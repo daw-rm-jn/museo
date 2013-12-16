b589561c26ce257a80599c393805a102d512d8a6
Yoyodin <javnue@gmail.com>
2013-12-16 03:42:32 +0100

    # Aumentado el tamaño del campo descripcionEstilo, habrá que ahcerlo con más... # Creadas unas cuántas sentencias INSERT # Actualizado modelo E/R

--- ScriptsBD/scripBDMuseo.sql
+++ ScriptsBD/scripBDMuseo.sql
@@ -1,304 +1,328 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `bd_Museo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `bd_Museo` ;
-
--- -----------------------------------------------------
--- Table `bd_Museo`.`Administrador`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Administrador` (
-  `email` VARCHAR(50) NOT NULL ,
-  `clave` VARCHAR(100) NOT NULL ,
-  `fechaAlta` DATE NOT NULL ,
-  PRIMARY KEY (`email`) )
-ENGINE = InnoDB;
+CREATE SCHEMA IF NOT EXISTS `bd_museo` DEFAULT CHARACTER SET latin1 ;
+USE `bd_museo` ;
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Pintor`
+-- Table `bd_museo`.`administrador`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Pintor` (
-  `idPintor` INT NOT NULL AUTO_INCREMENT ,
-  `nombrePintor` VARCHAR(50) NULL ,
-  `bioPintor` VARCHAR(4000) NULL ,
-  `fechaNacimiento` DATE NOT NULL ,
-  `fechaMuerte` DATE NULL ,
-  `fotoPintor` VARCHAR(150) NULL ,
-  PRIMARY KEY (`idPintor`) )
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `bd_museo`.`administrador` (
+  `email` VARCHAR(50) NOT NULL,
+  `clave` VARCHAR(100) NOT NULL,
+  `fechaAlta` DATE NOT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Planta_Museo`
+-- Table `bd_museo`.`actualizacion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Planta_Museo` (
-  `idPlanta` INT NOT NULL AUTO_INCREMENT ,
-  `numeroPlanta` INT(2) NOT NULL ,
-  `capacidad` INT(3) NOT NULL ,
-  PRIMARY KEY (`idPlanta`) )
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `bd_museo`.`actualizacion` (
+  `idActualizacion` INT(11) NOT NULL AUTO_INCREMENT,
+  `tituloActualizacion` VARCHAR(50) NOT NULL,
+  `fechaActualizacion` DATE NOT NULL,
+  `descActualizacion` VARCHAR(200) NOT NULL,
+  `Usuario_email` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`idActualizacion`),
+  INDEX `fk_Actualizacion_Usuario1` (`Usuario_email` ASC),
+  CONSTRAINT `fk_Actualizacion_Usuario1`
+    FOREIGN KEY (`Usuario_email`)
+    REFERENCES `bd_museo`.`administrador` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Sala_Museo`
+-- Table `bd_museo`.`usuario`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Sala_Museo` (
-  `idSala` INT NOT NULL AUTO_INCREMENT ,
-  `idPlanta` INT NOT NULL ,
-  `nombreSala` VARCHAR(45) NOT NULL ,
-  `descripcionSala` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idSala`) ,
-  INDEX `fk_Sala_Museo_Planta_Museo1` (`idPlanta` ASC) ,
+CREATE TABLE IF NOT EXISTS `bd_museo`.`usuario` (
+  `email` VARCHAR(50) NOT NULL,
+  `clave` VARCHAR(100) NOT NULL,
+  `nombre` VARCHAR(20) NULL DEFAULT NULL,
+  `nif` VARCHAR(10) NOT NULL,
+  `dir` VARCHAR(50) NULL DEFAULT NULL,
+  `cp` INT(11) NULL DEFAULT NULL,
+  `telf` INT(11) NULL DEFAULT NULL,
+  `fechaAlta` DATE NOT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `bd_museo`.`carrito`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_museo`.`carrito` (
+  `idCarrito` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(50) NOT NULL,
+  `fechaCreacion` DATE NOT NULL,
+  `fechaExpir` DATE NULL DEFAULT NULL,
+  PRIMARY KEY (`idCarrito`),
+  INDEX `fk_Carrito_Usuario1` (`email` ASC),
+  CONSTRAINT `fk_Carrito_Usuario1`
+    FOREIGN KEY (`email`)
+    REFERENCES `bd_museo`.`usuario` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `bd_museo`.`copia_cuadro`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_museo`.`copia_cuadro` (
+  `idCopia_Cuadro` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombreProducto` VARCHAR(100) NULL DEFAULT NULL,
+  `autor` VARCHAR(50) NULL DEFAULT NULL,
+  `estilo` VARCHAR(50) NULL DEFAULT NULL,
+  `fechaAlta` DATE NOT NULL,
+  `descripcion` VARCHAR(200) NULL DEFAULT NULL,
+  `precio` INT(11) NOT NULL,
+  `fotoCuadro` VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY (`idCopia_Cuadro`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `bd_museo`.`estilo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_museo`.`estilo` (
+  `idEstilo` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombreEstilo` VARCHAR(50) NULL DEFAULT NULL,
+  `descripcionEstilo` VARCHAR(3000) NULL DEFAULT NULL,
+  PRIMARY KEY (`idEstilo`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `bd_museo`.`planta_museo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_museo`.`planta_museo` (
+  `idPlanta` INT(11) NOT NULL AUTO_INCREMENT,
+  `numeroPlanta` INT(2) NOT NULL,
+  `capacidad` INT(3) NOT NULL,
+  PRIMARY KEY (`idPlanta`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `bd_museo`.`sala_museo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bd_museo`.`sala_museo` (
+  `idSala` INT(11) NOT NULL AUTO_INCREMENT,
+  `idPlanta` INT(11) NOT NULL,
+  `nombreSala` VARCHAR(45) NOT NULL,
+  `descripcionSala` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idSala`),
+  INDEX `fk_Sala_Museo_Planta_Museo1` (`idPlanta` ASC),
   CONSTRAINT `fk_Sala_Museo_Planta_Museo1`
-    FOREIGN KEY (`idPlanta` )
-    REFERENCES `bd_Museo`.`Planta_Museo` (`idPlanta` )
+    FOREIGN KEY (`idPlanta`)
+    REFERENCES `bd_museo`.`planta_museo` (`idPlanta`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Exposicion`
+-- Table `bd_museo`.`exposicion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Exposicion` (
-  `idExposicion` INT NOT NULL AUTO_INCREMENT ,
-  `idSala` INT NOT NULL ,
-  `nombreExposicion` VARCHAR(50) NULL ,
-  `fechaInicio` DATE NULL ,
-  `fechaFIn` DATE NULL ,
-  `descripcionExpo` VARCHAR(100) NULL ,
-  PRIMARY KEY (`idExposicion`) ,
-  INDEX `fk_Exposicion_Sala_Museo1` (`idSala` ASC) ,
+CREATE TABLE IF NOT EXISTS `bd_museo`.`exposicion` (
+  `idExposicion` INT(11) NOT NULL AUTO_INCREMENT,
+  `idSala` INT(11) NOT NULL,
+  `nombreExposicion` VARCHAR(50) NULL DEFAULT NULL,
+  `fechaInicio` DATE NULL DEFAULT NULL,
+  `fechaFIn` DATE NULL DEFAULT NULL,
+  `descripcionExpo` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`idExposicion`),
+  INDEX `fk_Exposicion_Sala_Museo1` (`idSala` ASC),
   CONSTRAINT `fk_Exposicion_Sala_Museo1`
-    FOREIGN KEY (`idSala` )
-    REFERENCES `bd_Museo`.`Sala_Museo` (`idSala` )
+    FOREIGN KEY (`idSala`)
+    REFERENCES `bd_museo`.`sala_museo` (`idSala`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Estilo`
+-- Table `bd_museo`.`pintor`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Estilo` (
-  `idEstilo` INT NOT NULL AUTO_INCREMENT ,
-  `nombreEstilo` VARCHAR(50) NULL ,
-  `descripcionEstilo` VARCHAR(100) NULL ,
-  PRIMARY KEY (`idEstilo`) )
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `bd_museo`.`pintor` (
+  `idPintor` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombrePintor` VARCHAR(50) NULL DEFAULT NULL,
+  `bioPintor` VARCHAR(4000) NULL DEFAULT NULL,
+  `fechaNacimiento` DATE NOT NULL,
+  `fechaMuerte` DATE NULL DEFAULT NULL,
+  `fotoPintor` VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY (`idPintor`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Cuadro`
+-- Table `bd_museo`.`cuadro`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Cuadro` (
-  `idCuadro` INT NOT NULL AUTO_INCREMENT ,
-  `idPintor` INT NOT NULL ,
-  `idExposicion` INT NOT NULL ,
-  `idEstilo` INT NOT NULL ,
-  `nombreCuadro` VARCHAR(100) NULL ,
-  `descripcionCuadro` VARCHAR(100) NULL ,
-  `fotoCuadro` VARCHAR(150) NULL ,
-  PRIMARY KEY (`idCuadro`) ,
-  INDEX `fk_Cuadro_Pintor` (`idPintor` ASC) ,
-  INDEX `fk_Cuadro_Exposicion1` (`idExposicion` ASC) ,
-  INDEX `fk_Cuadro_Estilo1` (`idEstilo` ASC) ,
-  CONSTRAINT `fk_Cuadro_Pintor`
-    FOREIGN KEY (`idPintor` )
-    REFERENCES `bd_Museo`.`Pintor` (`idPintor` )
+CREATE TABLE IF NOT EXISTS `bd_museo`.`cuadro` (
+  `idCuadro` INT(11) NOT NULL AUTO_INCREMENT,
+  `idPintor` INT(11) NOT NULL,
+  `idExposicion` INT(11) NOT NULL,
+  `idEstilo` INT(11) NOT NULL,
+  `nombreCuadro` VARCHAR(100) NULL DEFAULT NULL,
+  `descripcionCuadro` VARCHAR(100) NULL DEFAULT NULL,
+  `fotoCuadro` VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY (`idCuadro`),
+  INDEX `fk_Cuadro_Pintor` (`idPintor` ASC),
+  INDEX `fk_Cuadro_Exposicion1` (`idExposicion` ASC),
+  INDEX `fk_Cuadro_Estilo1` (`idEstilo` ASC),
+  CONSTRAINT `fk_Cuadro_Estilo1`
+    FOREIGN KEY (`idEstilo`)
+    REFERENCES `bd_museo`.`estilo` (`idEstilo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cuadro_Exposicion1`
-    FOREIGN KEY (`idExposicion` )
-    REFERENCES `bd_Museo`.`Exposicion` (`idExposicion` )
+    FOREIGN KEY (`idExposicion`)
+    REFERENCES `bd_museo`.`exposicion` (`idExposicion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Cuadro_Estilo1`
-    FOREIGN KEY (`idEstilo` )
-    REFERENCES `bd_Museo`.`Estilo` (`idEstilo` )
+  CONSTRAINT `fk_Cuadro_Pintor`
+    FOREIGN KEY (`idPintor`)
+    REFERENCES `bd_museo`.`pintor` (`idPintor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 20
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Copia_Cuadro`
+-- Table `bd_museo`.`datos_bancarios`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Copia_Cuadro` (
-  `idCopia_Cuadro` INT NOT NULL AUTO_INCREMENT ,
-  `nombreProducto` VARCHAR(100) NULL ,
-  `autor` VARCHAR(50) NULL ,
-  `estilo` VARCHAR(50) NULL ,
-  `fechaAlta` DATE NOT NULL ,
-  `descripcion` VARCHAR(200) NULL ,
-  `precio` INT NOT NULL ,
-  `fotoCuadro` VARCHAR(150) NULL ,
-  PRIMARY KEY (`idCopia_Cuadro`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `bd_Museo`.`Usuario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Usuario` (
-  `email` VARCHAR(50) NOT NULL ,
-  `clave` VARCHAR(100) NOT NULL ,
-  `nombre` VARCHAR(20) NULL ,
-  `nif` VARCHAR(10) NOT NULL ,
-  `dir` VARCHAR(50) NULL ,
-  `cp` INT NULL ,
-  `telf` INT NULL ,
-  `fechaAlta` DATE NOT NULL ,
-  PRIMARY KEY (`email`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `bd_Museo`.`Carrito`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Carrito` (
-  `idCarrito` INT NOT NULL AUTO_INCREMENT ,
-  `email` VARCHAR(50) NOT NULL ,
-  `fechaCreacion` DATE NOT NULL ,
-  `fechaExpir` DATE NULL ,
-  PRIMARY KEY (`idCarrito`) ,
-  INDEX `fk_Carrito_Usuario1` (`email` ASC) ,
-  CONSTRAINT `fk_Carrito_Usuario1`
-    FOREIGN KEY (`email` )
-    REFERENCES `bd_Museo`.`Usuario` (`email` )
+CREATE TABLE IF NOT EXISTS `bd_museo`.`datos_bancarios` (
+  `email` VARCHAR(50) NOT NULL,
+  `numeroTarjeta` BIGINT(20) NOT NULL,
+  `CCV` INT(11) NOT NULL,
+  `fechaCaducidad` DATE NOT NULL,
+  PRIMARY KEY (`numeroTarjeta`, `CCV`),
+  INDEX `fk_Datos_Bancarios_Usuario1` (`email` ASC),
+  CONSTRAINT `fk_Datos_Bancarios_Usuario1`
+    FOREIGN KEY (`email`)
+    REFERENCES `bd_museo`.`usuario` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Linea_Carrito`
+-- Table `bd_museo`.`linea_carrito`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Linea_Carrito` (
-  `idLinea_Carrito` INT NOT NULL,
-  `idCarrito` INT NOT NULL ,
-  `idCopia_Cuadro` INT NOT NULL ,
-  `nombreProducto` VARCHAR(50) NULL ,
-  `unidades` INT NULL ,
-  `precio` INT NULL ,
-  `IVA` INT NOT NULL ,
-  `totalLinea` INT NULL ,
-  PRIMARY KEY (`idLinea_Carrito`, `idCarrito`) ,
-  INDEX `fk_Linea_Carrito_Carrito1` (`idCarrito` ASC) ,
-  INDEX `fk_Linea_Carrito_Copia_Cuadro1` (`idCopia_Cuadro` ASC) ,
+CREATE TABLE IF NOT EXISTS `bd_museo`.`linea_carrito` (
+  `idLinea_Carrito` INT(11) NOT NULL,
+  `idCarrito` INT(11) NOT NULL,
+  `idCopia_Cuadro` INT(11) NOT NULL,
+  `nombreProducto` VARCHAR(50) NULL DEFAULT NULL,
+  `unidades` INT(11) NULL DEFAULT NULL,
+  `precio` INT(11) NULL DEFAULT NULL,
+  `IVA` INT(11) NOT NULL,
+  `totalLinea` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`idLinea_Carrito`, `idCarrito`),
+  INDEX `fk_Linea_Carrito_Carrito1` (`idCarrito` ASC),
+  INDEX `fk_Linea_Carrito_Copia_Cuadro1` (`idCopia_Cuadro` ASC),
   CONSTRAINT `fk_Linea_Carrito_Carrito1`
-    FOREIGN KEY (`idCarrito` )
-    REFERENCES `bd_Museo`.`Carrito` (`idCarrito` )
+    FOREIGN KEY (`idCarrito`)
+    REFERENCES `bd_museo`.`carrito` (`idCarrito`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Linea_Carrito_Copia_Cuadro1`
-    FOREIGN KEY (`idCopia_Cuadro` )
-    REFERENCES `bd_Museo`.`Copia_Cuadro` (`idCopia_Cuadro` )
+    FOREIGN KEY (`idCopia_Cuadro`)
+    REFERENCES `bd_museo`.`copia_cuadro` (`idCopia_Cuadro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Datos_Bancarios`
+-- Table `bd_museo`.`pedido`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Datos_Bancarios` (
-  `email` VARCHAR(50) NOT NULL ,
-  `numeroTarjeta` BIGINT NOT NULL ,
-  `CCV` INT NOT NULL ,
-  `fechaCaducidad` DATE NOT NULL ,
-  INDEX `fk_Datos_Bancarios_Usuario1` (`email` ASC) ,
-  PRIMARY KEY (`numeroTarjeta`, `CCV`) ,
-  CONSTRAINT `fk_Datos_Bancarios_Usuario1`
-    FOREIGN KEY (`email` )
-    REFERENCES `bd_Museo`.`Usuario` (`email` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `bd_Museo`.`Pedido`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Pedido` (
-  `email` VARCHAR(50) NOT NULL ,
-  `idPedido` INT NOT NULL AUTO_INCREMENT ,
-  `fecha` DATE NULL ,
-  `precioTotal` INT NULL ,
-  `estado` VARCHAR(45) NULL ,
-  INDEX `fk_Pedido_Usuario1` (`email` ASC) ,
-  PRIMARY KEY (`idPedido`) ,
+CREATE TABLE IF NOT EXISTS `bd_museo`.`pedido` (
+  `email` VARCHAR(50) NOT NULL,
+  `idPedido` INT(11) NOT NULL AUTO_INCREMENT,
+  `fecha` DATE NULL DEFAULT NULL,
+  `precioTotal` INT(11) NULL DEFAULT NULL,
+  `estado` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idPedido`),
+  INDEX `fk_Pedido_Usuario1` (`email` ASC),
   CONSTRAINT `fk_Pedido_Usuario1`
-    FOREIGN KEY (`email` )
-    REFERENCES `bd_Museo`.`Usuario` (`email` )
+    FOREIGN KEY (`email`)
+    REFERENCES `bd_museo`.`usuario` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Linea_Pedido`
+-- Table `bd_museo`.`linea_pedido`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Linea_Pedido` (
-  `idLinea_Pedido` INT NOT NULL ,
-  `idPedido` INT NOT NULL ,
-  `idCopia_Cuadro` INT NOT NULL ,
-  `nombreProducto` VARCHAR(50) NULL ,
-  `unidades` INT NULL ,
-  `precio` INT NULL ,
-  `IVA` INT NULL ,
-  `totalLinea` INT NULL ,
-  INDEX `fk_LInea_Pedido_Pedido1` (`idPedido` ASC) ,
-  INDEX `fk_LInea_Pedido_Copia_Cuadro1` (`idCopia_Cuadro` ASC) ,
-  CONSTRAINT `fk_LInea_Pedido_Pedido1`
-    FOREIGN KEY (`idPedido` )
-    REFERENCES `bd_Museo`.`Pedido` (`idPedido` )
+CREATE TABLE IF NOT EXISTS `bd_museo`.`linea_pedido` (
+  `idLinea_Pedido` INT(11) NOT NULL,
+  `idPedido` INT(11) NOT NULL,
+  `idCopia_Cuadro` INT(11) NOT NULL,
+  `nombreProducto` VARCHAR(50) NULL DEFAULT NULL,
+  `unidades` INT(11) NULL DEFAULT NULL,
+  `precio` INT(11) NULL DEFAULT NULL,
+  `IVA` INT(11) NULL DEFAULT NULL,
+  `totalLinea` INT(11) NULL DEFAULT NULL,
+  INDEX `fk_LInea_Pedido_Pedido1` (`idPedido` ASC),
+  INDEX `fk_LInea_Pedido_Copia_Cuadro1` (`idCopia_Cuadro` ASC),
+  CONSTRAINT `fk_LInea_Pedido_Copia_Cuadro1`
+    FOREIGN KEY (`idCopia_Cuadro`)
+    REFERENCES `bd_museo`.`copia_cuadro` (`idCopia_Cuadro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_LInea_Pedido_Copia_Cuadro1`
-    FOREIGN KEY (`idCopia_Cuadro` )
-    REFERENCES `bd_Museo`.`Copia_Cuadro` (`idCopia_Cuadro` )
+  CONSTRAINT `fk_LInea_Pedido_Pedido1`
+    FOREIGN KEY (`idPedido`)
+    REFERENCES `bd_museo`.`pedido` (`idPedido`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `bd_Museo`.`Recibo`
+-- Table `bd_museo`.`recibo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Recibo` (
-  `idRecibo` INT NOT NULL AUTO_INCREMENT ,
-  `idPedido` INT NOT NULL ,
-  `reciboHTML` VARCHAR(4000) NULL ,
-  PRIMARY KEY (`idRecibo`) ,
-  INDEX `fk_Recibo_Pedido1` (`idPedido` ASC) ,
+CREATE TABLE IF NOT EXISTS `bd_museo`.`recibo` (
+  `idRecibo` INT(11) NOT NULL AUTO_INCREMENT,
+  `idPedido` INT(11) NOT NULL,
+  `reciboHTML` VARCHAR(4000) NULL DEFAULT NULL,
+  PRIMARY KEY (`idRecibo`),
+  INDEX `fk_Recibo_Pedido1` (`idPedido` ASC),
   CONSTRAINT `fk_Recibo_Pedido1`
-    FOREIGN KEY (`idPedido` )
-    REFERENCES `bd_Museo`.`Pedido` (`idPedido` )
+    FOREIGN KEY (`idPedido`)
+    REFERENCES `bd_museo`.`pedido` (`idPedido`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `bd_Museo`.`Actualizacion`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `bd_Museo`.`Actualizacion` (
-  `idActualizacion` INT NOT NULL AUTO_INCREMENT ,
-  `tituloActualizacion` VARCHAR(50) NOT NULL ,
-  `fechaActualizacion` DATE NOT NULL ,
-  `descActualizacion` VARCHAR(200) NOT NULL ,
-  `Usuario_email` VARCHAR(50) NOT NULL ,
-  PRIMARY KEY (`idActualizacion`) ,
-  INDEX `fk_Actualizacion_Usuario1` (`Usuario_email` ASC) ,
-  CONSTRAINT `fk_Actualizacion_Usuario1`
-    FOREIGN KEY (`Usuario_email` )
-    REFERENCES `bd_Museo`.`Usuario` (`email` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
--- ScriptsBD/scripts_insert.sql
+++ ScriptsBD/scripts_insert.sql
@@ -1,5 +1,49 @@
 use bd_Museo;
 
-insert into Planta_Museo(numeroPlanta, capacidad) values (1,400);
-insert into Sala_Museo (idPlanta, nombreSala, descripcionSala) values (1,'salaprincipal','sala principal');
-insert into Exposicion (idSala,nombreExposicion,fechaInicio,fechaFin,descripcionExpo) values (1,'exposicion permanente',NOW(),NULL,'exposicion residente del museo');
+INSERT INTO Administrador(email,clave,fechaAlta) VALUES ('admin@admin.com','250939a0f3f4b5513d6f5335bc9c9550',NOW()); /*la clave está cifrada, es 'clavemd5'*/
+
+INSERT INTO Usuario(email,clave,nombre,nif,cp,telf,fechaAlta) VALUES ('cliente1@prueba.com','d5a8d8c7ab0514e2b8a2f98769281585','Cliente1','50948753Y',28009,666666666,NOW()); /*la clave es 'cliente1'*/
+INSERT INTO Usuario(email,clave,nombre,nif,cp,telf,fechaAlta) VALUES ('cliente2@prueba.com','6dcd0e14f89d67e397b9f52bb63f5570','Cliente2','02248123H',27033,585474696,NOW()); /*la clave es 'cliente2'*/
+INSERT INTO Usuario(email,clave,nombre,nif,cp,telf,fechaAlta) VALUES ('cliente3@prueba.com','428e859901e1b27ec01c7921afc31d98','Cliente3','84695442F',20000,147258369,NOW()); /*la clave es 'cliente3'*/
+
+INSERT INTO Carrito(email,fechaCreacion,fechaExpir) VALUES ('cliente1@prueba.com',NOW(),NOW()+INTERVAL 10 DAY);
+INSERT INTO Carrito(email,fechaCreacion,fechaExpir) VALUES ('cliente2@prueba.com',NOW(),NOW()+INTERVAL 10 DAY);
+INSERT INTO Carrito(email,fechaCreacion,fechaExpir) VALUES ('cliente3@prueba.com',NOW(),NOW()+INTERVAL 10 DAY);
+
+INSERT INTO Copia_Cuadro(nombreProducto,autor,estilo,fechaAlta,descripcion,precio,fotoCuadro) VALUES ();
+INSERT INTO Copia_Cuadro(nombreProducto,autor,estilo,fechaAlta,descripcion,precio,fotoCuadro) VALUES ();
+INSERT INTO Copia_Cuadro(nombreProducto,autor,estilo,fechaAlta,descripcion,precio,fotoCuadro) VALUES ();
+
+INSERT INTO Estilo(nombreEstilo,descripcionEstilo) VALUES ('Barroco','La pintura barroca es la pintura relacionada con el movimiento cultural barroco. El movimiento a menudo se le identifica con el absolutismo, la Contrarreforma y el renacimiento católico, pero la existencia de importante arte y arquitectura barrocos en países no absolutistas y protestantes por toda Europa Occidental evidencian su amplia popularidad.');
+INSERT INTO Estilo(nombreEstilo,descripcionEstilo) VALUES ('Expresionismo','El expresionismo fue un movimiento cultural surgido en Alemania a principios del siglo XX, que tuvo plasmación en un gran número de campos: artes plásticas, literatura, música, cine, teatro, danza, fotografía, etc.');
+INSERT INTO Estilo(nombreEstilo,descripcionEstilo) VALUES ('Cubismo','El cubismo fue un movimiento artístico desarrollado entre 1907 y 1914, nacido en Francia y encabezado por Pablo Picasso, Georges Braque y Juan Gris. Es una tendencia esencial, pues da pie al resto de las vanguardias europeas del siglo XX. No se trata de un ismo más, sino de la ruptura definitiva con la pintura tradicional.');
+
+INSERT INTO Pintor(nombrePintor,bioPintor,fechaNacimiento,fechaMuerte,fotoPintor) VALUES ();
+INSERT INTO Pintor(nombrePintor,bioPintor,fechaNacimiento,fechaMuerte,fotoPintor) VALUES ();
+INSERT INTO Pintor(nombrePintor,bioPintor,fechaNacimiento,fechaMuerte,fotoPintor) VALUES ();
+
+INSERT INTO Cuadro(idPintor,idExposicion,idEstilo,nombreCuadro,descripcionCuadro,fotoCuadro) VALUES ();
+INSERT INTO Cuadro(idPintor,idExposicion,idEstilo,nombreCuadro,descripcionCuadro,fotoCuadro) VALUES ();
+INSERT INTO Cuadro(idPintor,idExposicion,idEstilo,nombreCuadro,descripcionCuadro,fotoCuadro) VALUES ();
+
+INSERT INTO Datos_Bancarios(email,numeroTarjeta,CCV,fechaCaducidad) VALUES ('cliente1@prueba.com',0123456789123456,753,2017-01-01);
+INSERT INTO Datos_Bancarios(email,numeroTarjeta,CCV,fechaCaducidad) VALUES ('cliente2@prueba.com',6543219876543210,357,2017-01-01);
+INSERT INTO Datos_Bancarios(email,numeroTarjeta,CCV,fechaCaducidad) VALUES ('cliente3@prueba.com',1472583690963852,456,2018-05-01);
+
+INSERT INTO Linea_Carrito(idLinea_Carrito,idCarrito,idCopia_Cuadro,nombreProducto,unidades,precio,IVA,totalLinea) VALUES ();
+INSERT INTO Linea_Carrito(idLinea_Carrito,idCarrito,idCopia_Cuadro,nombreProducto,unidades,precio,IVA,totalLinea) VALUES ();
+INSERT INTO Linea_Carrito(idLinea_Carrito,idCarrito,idCopia_Cuadro,nombreProducto,unidades,precio,IVA,totalLinea) VALUES ();
+
+INSERT INTO Pedido(email,fecha,precioTotal,estado) VALUES ('cliente1@prueba.com',2011-06-26,105,'Entregado');
+INSERT INTO Pedido(email,fecha,precioTotal,estado) VALUES ('cliente2@prueba.com',2013-05-21,82,'Entregado');
+INSERT INTO Pedido(email,fecha,precioTotal,estado) VALUES ('cliente3@prueba.com',NOW(),'En Espera');
+
+INSERT INTO Linea_Pedido(idLinea_Pedido,idPedido,idCopia_Cuadro,nombreProducto,unidades,precio,IVA,totalLinea) VALUES ();
+INSERT INTO Linea_Pedido(idLinea_Pedido,idPedido,idCopia_Cuadro,nombreProducto,unidades,precio,IVA,totalLinea) VALUES ();
+INSERT INTO Linea_Pedido(idLinea_Pedido,idPedido,idCopia_Cuadro,nombreProducto,unidades,precio,IVA,totalLinea) VALUES ();
+
+INSERT INTO Recibo(idRecibo,idPedido,reciboHTML) VALUES ();
+
+INSERT INTO Planta_Museo(numeroPlanta, capacidad) VALUES (1,400);
+INSERT INTO Sala_Museo (idPlanta, nombreSala, descripcionSala) VALUES (1,'salaprincipal','sala principal');
+INSERT INTO Exposicion (idSala,nombreExposicion,fechaInicio,fechaFin,descripcionExpo) VALUES (1,'exposicion permanente',NOW(),NULL,'exposicion residente del museo');
